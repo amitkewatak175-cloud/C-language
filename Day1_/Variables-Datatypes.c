4da45cbfb39a5997ed51a91f236eb3f1b7e08139
Amit kewat <amitkewatak175@gmail.com>
2026-04-05 03:41:52 +0530

    Variable and datatypes

---
+++ Day1_/Variables-Datatypes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int a=5;
+    char b='A';
+    float c=5.0/2;
+    double d=15.0/4;
+
+    printf("Integer A = %d\n", a);
+    printf("Character b = %c\n", b);
+    printf("Float c = %f\n", c);
+    printf("Double d = %lf\n", d);
+    return 0;
+}
